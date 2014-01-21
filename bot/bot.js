e6f2696ca5464a8d4f033d191473b09c7a156529
walterhiggins <walter.higgins@gmail.com>
2014-01-21 23:28:49 +0000

    updated to comply with new commonjs module style

--- bot/bot.js
+++ bot/bot.js
@@ -7,80 +7,88 @@
   /jsp stay
   /jsp pack
 */
-load(__folder + "../core/scriptcraft.js");
+var _store = {players: {}};
 
-var bot = bot || plugin("bot",{
-  /* helper methods for a player's bot */
-  get_bot: function(player){
-		return this.store.players[player.name];
-	},
-	set_bot: function(player,bot,inventory){
-		this.store.players[player.name] = {bot:bot,inventory:inventory};
-	},
+var bot = plugin('bot',{
+    /* helper methods for a player's bot */
+    get_bot: function(player){
+        return _store.players[player.name];
+    },
+    set_bot: function(player,bot,inventory){
+        _store.players[player.name] = {bot:bot,inventory:inventory};
+    },
     /* summons your bot */
-    summon: function(){
-		var player = getPlayerObject();
-		var world  = player.getWorld();
-		
-		// in case you already have one spawned
-		this.dismiss();
-		
-		// place wolf two squares in front of you
-		var my_bot = world.spawnCreature(player.getLocation().add(0,0,2), org.bukkit.entity.EntityType.WOLF);
-		
-		my_bot.setTamed(true);
-		my_bot.setOwner(player);
-		my_bot.setTarget(player);
-		
-		var inventory; 
-		var b = this.get_bot(player);
-		if(b == null || b.inventory == null)
-			inventory = player.getServer().createInventory(player, 18, "Bot's Pack");
-		else
-			inventory = b.inventory;
-		
-		this.set_bot(player,my_bot,inventory);
-	},
+    summon: function(player){
+        var world  = player.world
+        
+        // in case you already have one spawned
+        this.dismiss(player);
+        
+        // place wolf two squares in front of you
+        var my_bot = world.spawnCreature(player.getLocation().add(0,0,2), org.bukkit.entity.EntityType.WOLF);
+        
+        my_bot.setTamed(true);
+        my_bot.setOwner(player);
+        my_bot.setTarget(player);
+        
+        var inventory; 
+        var b = this.get_bot(player);
+        if(b == null || b.inventory == null)
+            inventory = player.getServer().createInventory(player, 18, "Bot's Pack");
+        else
+            inventory = b.inventory;
+        
+        this.set_bot(player,my_bot,inventory);
+    },
     /* dismisses your bot */
-    dismiss: function(){
-		var b = this.get_bot(getPlayerObject());
-		if( b != null && b.bot !== null){
-			b.bot.remove();
-			b.bot = null;
-		}
-	},
-	/* instructs your bot to stay */
-	stay: function(){
-		var b = this.get_bot(getPlayerObject());
-		if(b!= null && b.bot !== null){
-			b.bot.setTarget(null);
-			b.bot.setSitting(true);
-		}
-	},
-	/* instructs your bot to follow you */
-	come: function(){
-		var b = this.get_bot(getPlayerObject());
-		if(b!= null){
-			b.bot.setSitting(false);
-			b.bot.setTarget(getPlayerObject());
-		}
-	},
-	/* instructs your bot to display its pack */
-	pack: function(){
-		var b = this.get_bot(getPlayerObject());
-		if(b!= null && b.bot !== null){
-			b.bot.setSitting(true);
-			getPlayerObject().openInventory(b.inventory);
-		}
-	}
+    dismiss: function(player){
+        var b = this.get_bot(player);
+        if( b != null && b.bot !== null){
+            b.bot.remove();
+            b.bot = null;
+        }
+    },
+    /* instructs your bot to stay */
+    stay: function(player){
+        var b = this.get_bot(player);
+        if(b!= null && b.bot !== null){
+            b.bot.setTarget(null);
+            b.bot.setSitting(true);
+        }
+    },
+    /* instructs your bot to follow you */
+    come: function(player){
+        var b = this.get_bot(player);
+        if(b!= null){
+            b.bot.setSitting(false);
+            b.bot.setTarget(player);
+        }
+    },
+    /* instructs your bot to display its pack */
+    pack: function(player){
+        var b = this.get_bot(player);
+        if(b!= null && b.bot !== null){
+            b.bot.setSitting(true);
+            player.openInventory(b.inventory);
+        }
+    },
+    store: _store
 },true);
 
-/* initialize data */
-bot.store.players = bot.store.players || {};
-ready(function(){
-	command("summon",function(){bot.summon();});
-	command("dismiss",function(){bot.dismiss();});
-	command("stay",function(){bot.stay();});
-	command("come",function(){bot.come();});
-	command("pack",function(){bot.pack();});
+exports.bot = bot;
+
+command('summon',function(params, sender){
+    bot.summon(sender);
+});
+command('dismiss',function(params, sender){
+    bot.dismiss(sender);
+});
+command('stay',function(params, sender){
+    bot.stay(sender);
+});
+command('come',function(params, sender){
+    bot.come(sender);
+});
+command('pack',function(params, sender){
+    bot.pack(sender);
 });
